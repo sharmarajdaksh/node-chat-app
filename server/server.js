373c99be41637da7741a75217c5d7c09f18044ee
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2019-06-11 12:58:50 +0530

    Added styling for chat app

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', (socket) => {
-        console.log('A user disconnected');
+        io.emit('newMessage', generateMessage('Admin', 'A user left the chat'));
     });
 });
 
